2f384fae9a76b2093b77351b6641a594ebbe5ba8
0xAckerMan <joelkores1@gmail.com>
2024-05-06 16:10:32 +0300

    Fix

--- fork-join/wait-groups/main.go
+++ fork-join/wait-groups/main.go
@@ -9,33 +9,36 @@ import (
 func main() {
 	now := time.Now()
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		task1()
-		task2()
-		task3()
-		task4()
-	}()
+	wg.Add(4)
+    go task1(&wg)
+    go task2(&wg)
+    go task3(&wg)
+    go task4(&wg)
+
 	wg.Wait()
 	fmt.Println("Elapsed, ", time.Since(now))
 }
 
-func task1() {
-	time.Sleep(100 * time.Millisecond)
+func task1(wg *sync.WaitGroup) {
+    defer wg.Done()
+	time.Sleep(500 * time.Millisecond)
 	fmt.Println("Task 1")
 }
 
-func task2() {
-	time.Sleep(400 * time.Millisecond)
+func task2(wg *sync.WaitGroup) {
+    defer wg.Done()
+	time.Sleep(600 * time.Millisecond)
 	fmt.Println("Task 2")
 }
 
-func task3() {
+func task3(wg *sync.WaitGroup) {
+    defer wg.Done()
+    time.Sleep(600*time.Millisecond)
 	fmt.Println("Task 3")
 }
 
-func task4() {
+func task4(wg *sync.WaitGroup) {
+    defer wg.Done()
 	time.Sleep(500 * time.Millisecond)
 	fmt.Println("Task 4")
 }
